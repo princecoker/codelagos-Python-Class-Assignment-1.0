ce0f99b3b3195e340ec4dd325b564948af408949
princecoker <40108833+princecoker@users.noreply.github.com>
2018-08-09 13:46:06 +0100

    Add files via upload

---
+++ out of school assignment 1.py
@@ -0,0 +1,5 @@
+a = float(input ('the value of a is \n'))
+b = float(input ('the value of b is \n'))
+c = float(input ('the value of c is \n'))
+x = -b + (4 * a * c) / (2 * a)
+print ('the value of x is \n',x)
---
+++ out of school assignment 2.py
@@ -0,0 +1,25 @@
+#this calculator tells what year you will be 100 years
+#algorithm
+#get name and age
+#calculate birthyear
+#add 100 to year of birth
+#display year in 100 years time
+
+name = str(input('what is your name: '))
+age = int(input('How old are you: '))
+birthyear = 2018 - age
+futuredate = int(birthyear + 100)
+print (name,'you will be 100 years in the year',futuredate)
+print ('\n', '\n', 'same program different approach below', '\n \n')
+
+
+#this calculator tells what year you will be 100 years
+#algorithm
+#get name and year of birth
+#add 100 to year of birth
+#display year in 100 years time
+
+name = str(input('what is your name: '))
+yearofbirth = int(input ('what year were you born: '))
+futuredate = int(yearofbirth + 100)
+print (name,'you will be 100 years in the year',futuredate)
